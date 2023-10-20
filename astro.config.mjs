f1d3e4b72d0bd3987c98d1e34c65f5e4abfe1dac
steve <stevebutler11@gmail.com>
2023-10-20 13:25:56 +0100

    CM3065 week 1 notes added

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,10 +1,14 @@
-import { defineConfig } from 'astro/config';
-import mdx from '@astrojs/mdx';
-import sitemap from '@astrojs/sitemap';
+import { defineConfig } from "astro/config";
+import mdx from "@astrojs/mdx";
+import sitemap from "@astrojs/sitemap";
 import react from "@astrojs/react";
 
 // https://astro.build/config
 export default defineConfig({
-  site: 'https://stevebutler.info',
-  integrations: [mdx(), sitemap(), react()]
+  site: "https://stevebutler.info",
+  integrations: [
+    mdx(),
+    sitemap(),
+    react(),
+  ],
 });
