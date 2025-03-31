44930786e435a2cae2601c52bb2f2eafa1f313be
Steven Butler <stevebutler11@gmail.com>
2025-03-31 08:19:44 +0100

    setup custom domain

--- astro.config.mjs
+++ astro.config.mjs
@@ -4,7 +4,7 @@ import sitemap from "@astrojs/sitemap";
 import tailwind from "@astrojs/tailwind";
 
 export default defineConfig({
-  site: "https://stevebutler11.github.io",
+  site: "https://stevebutler.info",
   base: "/stevebutler.info",
   integrations: [mdx(), sitemap(), tailwind()],
 });
